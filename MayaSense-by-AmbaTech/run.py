9f8b0e378eb4434d99b3a1f7c3d23e09b2299325
Rynix <92219166+Gaben69181@users.noreply.github.com>
2025-11-01 19:55:37 +0700

    Refactor run.py to include Flask and OpenAI setup
    Updated the run.py script to include necessary imports and configurations for the Flask application, OpenAI API, and audio processing.

--- MayaSense-by-AmbaTech/run.py
+++ MayaSense-by-AmbaTech/run.py
@@ -21,7 +21,7 @@ from Utility.promptMaker import *
 sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
 
 # use your own API Key, you can get it from https://openai.com/. I place my API Key in a separate file called config.py
-openai.api_key = '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'
+openai.api_key = ''
 
 conversation = []
 # Create a dictionary to hold the message data
@@ -155,4 +155,5 @@ if __name__ == "__main__":
                 record_audio()
 
     except KeyboardInterrupt:
+
         print("Stopped")
